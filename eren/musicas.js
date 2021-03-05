a9c359183f28aff6f7e488d926d8cafe943a43a9
Tio Gaara <80019958+gaarazinho0@users.noreply.github.com>
2021-03-05 08:19:00 -0300

    Add files via upload

---
+++ eren/musicas.js
@@ -0,0 +1,100 @@
+const musicas = () => { 
+	return `           
+──────────────────
+➣_YOUTUBE MUSIC_
+──────────────────
+➣_COMANDOS:_
+──────────────────
+➣𝘽𝙍'𝙨 🇧🇷
+══════════════════
+1• *${prefix}hylander*
+2• *${prefix}nikeb*
+3• *${prefix}BanhoDeLeite*
+4• *${prefix}AkDoFlamengo*
+5• *${prefix}Nemo*
+6• *${prefix}kalidade*
+7• *${prefix}macaverde*
+8• *${prefix}m4*
+9• *${prefix}cmgremio*
+10• *${prefix}cmflamengo*
+11• *${prefix}paypal*
+12• *${prefix}anotaaplaca*
+13• *${prefix}okbaby*
+14• *${prefix}dejavu*
+15• *${prefix}étãobom*
+──────────────────
+➣𝙄𝙉𝙏𝙀𝙍𝙉𝘼𝘾𝙄𝙊𝙉𝘼𝙇 🌍
+══════════════════
+1• *${prefix}daddyissues*
+2• *${prefix}sweaterweather*
+3• *${prefix}slowdancingunthedark*
+4• *${prefix}yeahright*
+5• *${prefix}physical*
+6• *${prefix}dontstartnow*
+
+•••
+──────────────────
+➣𝙇𝙄𝙇 𝙋𝙀𝙀𝙋 🕊️
+══════════════════
+1• *${prefix}starshopping*
+2• *${prefix}savethatshit*
+3• *${prefix}thebrightside*
+4• *${prefix}awfulthings*
+5• *${prefix}lifeisbeautiful*
+6• *${prefix}crybaby*
+7• *${prefix}runaway*
+8• *${prefix}beatit*
+9• *${prefix}spotlight*
+10• *${prefix}nuts*
+11• *${prefix}benztruck*
+12• *${prefix}favoritedress*
+13• *${prefix}fuckyoucunt*
+14• *${prefix}beamerboy*
+15• *${prefix}tribute*
+16• *${prefix}bigcityblues*
+17• *${prefix}fallingdown*
+18• *${prefix}avoid*
+19• *${prefix}witchblades*
+20• *${prefix}brokensmile*
+21• *${prefix}hellboy*
+22• *${prefix}ghostboy*
+23• *${prefix}belgium*
+──────────────────
+➣𝙓𝙓𝙓𝙏𝙀𝙉𝙏𝘼𝘾𝙄𝙊𝙉 🖤
+══════════════════
+1• *${prefix}fucklove*
+2• *${prefix}lookatme*
+3• *${prefix}moonlight*
+4• *${prefix}sad*
+5• *${prefix}revenge*
+6• *${prefix}theremedyforabrokenheart*
+7• *${prefix}jocelynflores*
+8• *${prefix}hope*
+
+•••
+──────────────────
+➣7𝙈𝙏𝙕 & 𝙈𝙃𝙍𝘼𝙋 🔥
+══════════════════
+1• *${prefix}rapdaakatsuki*
+2• *${prefix}rapdoshokages*
+3• *${prefix}rapdohidan*
+4• *${prefix}vibezabuza*
+
+•••
+──────────────────
+➣𝙉𝙎𝘾 🎶
+══════════════════
+1• *${prefix}shine*
+2• *${prefix}crazy*
+3• *${prefix}on_on*
+4• *${prefix}weakness*
+5• *${prefix}fearless*
+
+•••
+──────────────────
+『 SUGESTÃO DE MSC 』
+           
+_*wa.me/559684081026*_
+──────────────────`
+}
+exports.musicas = musicas
