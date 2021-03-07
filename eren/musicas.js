b45ea1fb12b53e926374c22cb8e3eec2e6302989
Tio Gaara <80019958+gaarazinho0@users.noreply.github.com>
2021-03-07 02:51:20 -0300

    Add files via upload

---
+++ eren/musicas.js
@@ -0,0 +1,136 @@
+const musicas = () => { 
+	return `           
+──────────────────
+➣_YOUTUBE MUSIC_
+──────────────────
+➣_COMANDOS:_
+──────────────────
+➣𝘽𝙍'𝙨 🇧🇷
+══════════════════
+1• *${prefix}hylander*
+2• *${prefix}nikeb*
+3• *${prefix}BanhoDeLeite*
+4• *${prefix}AkDoFlamengo*
+5• *${prefix}Nemo*
+6• *${prefix}kalidade*
+7• *${prefix}macaverde*
+8• *${prefix}m4*
+9• *${prefix}cmgremio*
+10• *${prefix}cmflamengo*
+11• *${prefix}paypal*
+12• *${prefix}anotaaplaca*
+13• *${prefix}okbaby*
+14• *${prefix}dejavu*
+15• *${prefix}étãobom*
+16• *${prefix}cabradapeste*
+17• *${prefix}777-666*
+18• *${prefix}atenatransa*
+──────────────────
+➣𝙄𝙉𝙏𝙀𝙍𝙉𝘼𝘾𝙄𝙊𝙉𝘼𝙇 🌍
+══════════════════
+1• *${prefix}daddyissues*
+2• *${prefix}sweaterweather*
+3• *${prefix}slowdancingunthedark*
+4• *${prefix}yeahright*
+5• *${prefix}physical*
+6• *${prefix}dontstartnow*
+7• *${prefix}astronautintheocean*
+8• *${prefix}mood*
+9• *${prefix}adoreyou*
+10• *${prefix}watermelonsugar*
+11• *${prefix}infinity*
+12• *${prefix}rockstar*
+
+•••
+──────────────────
+➣𝙇𝙄𝙇 𝙋𝙀𝙀𝙋 🕊️
+══════════════════
+1• *${prefix}starshopping*
+2• *${prefix}savethatshit*
+3• *${prefix}thebrightside*
+4• *${prefix}awfulthings*
+5• *${prefix}lifeisbeautiful*
+6• *${prefix}crybaby*
+7• *${prefix}runaway*
+8• *${prefix}beatit*
+9• *${prefix}spotlight*
+10• *${prefix}nuts*
+11• *${prefix}benztruck*
+12• *${prefix}favoritedress*
+13• *${prefix}fuckyoucunt*
+14• *${prefix}beamerboy*
+15• *${prefix}tribute*
+16• *${prefix}bigcityblues*
+17• *${prefix}fallingdown*
+18• *${prefix}avoid*
+19• *${prefix}witchblades*
+20• *${prefix}brokensmile*
+21• *${prefix}hellboy*
+22• *${prefix}ghostboy*
+23• *${prefix}belgium*
+24• *${prefix}16lines*
+──────────────────
+➣𝙓𝙓𝙓𝙏𝙀𝙉𝙏𝘼𝘾𝙄𝙊𝙉 🖤
+══════════════════
+1• *${prefix}fucklove*
+2• *${prefix}lookatme*
+3• *${prefix}moonlight*
+4• *${prefix}sad*
+5• *${prefix}revenge*
+6• *${prefix}theremedyforabrokenheart*
+7• *${prefix}jocelynflores*
+8• *${prefix}hope*
+
+•••
+──────────────────
+➣7𝙈𝙏𝙕 & 𝙈𝙃𝙍𝘼𝙋 🔥
+══════════════════
+1• *${prefix}rapdaakatsuki*    7𝙈𝙏𝙕
+2• *${prefix}rapdoshokages*   7𝙈𝙏𝙕
+3• *${prefix}rapdohidan*       7𝙈𝙏𝙕
+4• *${prefix}rapdokisame*      7𝙈𝙏𝙕
+5• *${prefix}rapdosasuke*      7𝙈𝙏𝙕
+6• *${prefix}rapdogaara*       7𝙈𝙏𝙕
+7• *${prefix}caixaodeareia*     7𝙈𝙏𝙕
+8• *${prefix}rapdonaruto*       7𝙈𝙏𝙕
+9• *${prefix}rapdoneji*          7𝙈𝙏𝙕
+10• *${prefix}rapdoedwardelric*7𝙈𝙏𝙕
+11• *${prefix}rapdogon*        7𝙈𝙏𝙕
+12• *${prefix}rapdoitachi*      7𝙈𝙏𝙕
+13• *${prefix}rapdoshikamaru* 7𝙈𝙏𝙕 
+14• *${prefix}marcadamaldição*7𝙈𝙏𝙕
+15• *${prefix}rapdolufy*        7𝙈𝙏𝙕 
+16• *${prefix}tefaltaodio*      7𝙈𝙏𝙕
+17• *${prefix}rapdoL*          7𝙈𝙏𝙕
+18• *${prefix}vibezabuza*    𝙈𝙃𝙍𝘼𝙋
+19• *${prefix}tipohashirama* 𝙈𝙃𝙍𝘼𝙋
+20• *${prefix}tipokakashi*    𝙈𝙃𝙍𝘼𝙋 
+21• *${prefix}tipohiruzen*    𝙈𝙃𝙍𝘼𝙋
+22• *${prefix}tipojiraiya*     𝙈𝙃𝙍𝘼𝙋
+23• *${prefix}tipokonan*     𝙈𝙃𝙍𝘼𝙋
+24• *${prefix}tipoL*          𝙈𝙃𝙍𝘼𝙋
+25• *${prefix}tipokira*       𝙈𝙃𝙍𝘼𝙋 
+26• *${prefix}tiponarutin*    𝙈𝙃𝙍𝘼𝙋 
+27• *${prefix}tipominato*    𝙈𝙃𝙍𝘼𝙋
+28• *${prefix}tipoobito*     𝙈𝙃𝙍𝘼𝙋
+29• *${prefix}tipopain*      𝙈𝙃𝙍𝘼𝙋
+30• *${prefix}tipoorochimaru*𝙈𝙃𝙍𝘼𝙋
+
+•••
+──────────────────
+➣𝙉𝙎𝘾 🎶
+══════════════════
+1• *${prefix}shine*
+2• *${prefix}crazy*
+3• *${prefix}on_on*
+4• *${prefix}weakness*
+5• *${prefix}fearless*
+
+•••
+──────────────────
+『 SUGESTÃO DE MSC 』
+           
+_*wa.me/559684081026*_
+──────────────────`
+}
+exports.musicas = musicas
